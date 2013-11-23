95ec3577b41886e81a8c902255b6451569138aa5
yu <402290527@qq.com>
2013-11-23 11:10:43 +0800

    tab5

---
+++ src/com/pangu/neusoft/healthe/TabActivity5.java
@@ -0,0 +1,17 @@
+package com.pangu.neusoft.healthe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TabActivity5 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab5_charge);
+	}
+
+	
+
+}
